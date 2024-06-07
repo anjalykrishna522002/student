17873b4423dffbe583cd8fce9a93939f0c9ef994
anjalykrishna522002 <anjalykrishnaas@gmail.com>
2024-06-07 12:55:59 +0530

    completed

---
+++ src/components/ViewStudent.jsx
@@ -0,0 +1,164 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewStudent = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div className="card" >
+                                    <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg" class="card-img-top" alt="..."></img>
+                                    <div className="card-body">
+                                        <h5 className="card-title">student</h5>
+                                        <p className="card-text">computer science</p>
+                                        <a href="#" class="btn btn-primary">JOHN</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewStudent
